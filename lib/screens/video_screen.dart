55cff27a1ea7141a9c78e57f892c2655d4ae7f07
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-10 15:39:55 +0200

    Draw VideoScreen layout
    - video player, title, viewCount, like/dislike,share, description

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -22,7 +22,7 @@ class VideoScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             YoutubePlayer(
               controller: _playerController,
@@ -31,9 +31,97 @@ class VideoScreen extends StatelessWidget {
                 print('Player is ready');
               },
             ),
+            SizedBox(height: 16.0),
+            _buildTitleAndViewCount(),
+            SizedBox(height: 16.0),
+            Divider(height: 0.0, color: Colors.black12),
+            SizedBox(height: 8.0),
+            _buildActionsButtonsRow(),
+            SizedBox(height: 8.0),
+            Divider(height: 0.0, color: Colors.black12),
+            SizedBox(height: 16.0),
+            _buildDescriptionContainer(),
+            SizedBox(height: 100),
           ],
         ),
       ),
+    );
+  }
+
+  Container _buildTitleAndViewCount() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  video.title,
+                  style: TextStyle(color: Colors.black, fontSize: 18.0),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Text("${video.minInfo.viewCount} views"),
+        ],
+      ),
+    );
+  }
+
+  Container _buildDescriptionContainer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
+      child: Text('video.description'),
+    );
+  }
+
+  Row _buildActionsButtonsRow() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                child: Icon(Icons.thumb_up),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                child: Text("${int.parse('31')}"),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Icon(Icons.thumb_down),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                child: Text("${int.parse('31')}"),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Icon(Icons.share),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                child: Text("Share"),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
